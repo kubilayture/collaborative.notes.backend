bc000dbd7afe8c10cfadac4c3fcb1ef7c502ce1e
kubilayture <kubilayture@gmail.com>
2025-08-29 13:38:45 +0300

    Update default port from 3000 to 4000 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port);
 
   console.log(`🚀 Application is running on: http://localhost:${port}`);
