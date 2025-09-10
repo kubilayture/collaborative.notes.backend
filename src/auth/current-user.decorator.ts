42899237dd46ea5dc1aeabc4d530e8346577fec6
kubilay türe <kubilayture@gmail.com>
2025-09-10 23:45:15 +0300

    updated current user decorator

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -3,7 +3,13 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const user = request.user;
+
+    if (data && user) {
+      return user[data];
+    }
+
+    return user;
   },
 );
 
